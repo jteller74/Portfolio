5ee384d9835b7a4c9ae52759ce4a516ae2c7bf96
Jared Teller <17jaredt17@gmail.com>
2020-05-04 22:34:24 -0400

    updated projects page

--- src/Projects.js
+++ src/Projects.js
@@ -4,28 +4,32 @@ import {
   Container, Row, Col, Card, Button, CardImg, CardTitle, CardText, CardDeck,
   CardSubtitle, CardBody
 } from 'reactstrap';
+import hashPicture from './images/hashmap.png';
+import shellPic from './images/shellthumbnail.jpg'
+import snackPic from './images/snackproj.png';
+import hisser from './images/hisserproj.png';
+import youtubeImage from './images/youtube.png'
 
 class Project extends Component {
     render() {
         return ( //the container is a bootstrap container for responsive layouts
-            <Container fluid>
-                
+            <Container fluid style={{backgroundColor: "rgb(46,49,65)"}}>
+            <h4 style={{color: 'white'}}>Projects</h4>
+            <Row>
+                <Col md="4" sm="6">
+                    <Preview title='Custom Shell' text="A custom shell written in C that features Finite State Machine design, Make, lexical analysis (tokenization), file redirection, and Pipes. Built within Windows Subsystem for Linux and Visual Studio Code." imageLink={shellPic} />
+                    <Preview title='Hisser Sharing' text="A twitter clone created using PHP, MySQLi, HTML, Bootstrap, and CSS. Features custom profiles, user feeds, posts, and searching." imageLink={ hisser } />
+                </Col>
 
-            <CardDeck>
-                <Preview title='test' subtitle="Testing this out" text="asdfdsfdsddddddddddddddddddddddddddddddddddddddddddddddddddddddd teller"/>
-                <Preview title='test' subtitle="Testing this out" text="asdfdsfdsddddddddddddddddddddddddddddddddddddddddddddddddddddddd teller"/>
-                <Preview title='test' subtitle="Testing this out" text="asdfdsfdsddddddddddddddddddddddddddddddddddddddddddddddddddddddd teller"/>
+                <Col md="4" sm="6">
+                    <Preview title='Snack Finder' text="A Python (Django) web application for finding local snacks. Features authentication, PostgreSQL, MVC design, and Bootstrap." link="https://github.com/jaredt17/CacheSimulator" imageLink={ snackPic } />
+                    <Preview title='YouTube Channel' text="My YouTube channel has about 650 Subscribers and over 125,000 views! I generally share my custom scripts for video games written in AutoHotkey scripting language." imageLink={ youtubeImage } />
+                </Col>
 
-                <Preview title='test' subtitle="Testing this out" text="asdfdsfdsddddddddddddddddddddddddddddddddddddddddddddddddddddddd teller"/>
-            </CardDeck>
-
-            <CardDeck>
-            <Preview title='test' subtitle="Testing this out" text="asdfdsfdsddddddddddddddddddddddddddddddddddddddddddddddddddddddd teller"/>
-                <Preview title='test' subtitle="Testing this out" text="asdfdsfdsddddddddddddddddddddddddddddddddddddddddddddddddddddddd teller"/>
-
-                <Preview title='Testing Title' subtitle="Testing this out" text="asdfdsfdsddddddddddddddddddddddddddddddddddddddddddddddddddddddd teller" link="https://github.com/jaredt17/CacheSimulator"/>
-            </CardDeck>
-
+                <Col md="4" sm="6">
+                <Preview title='Hashing Based Search Engine' text="A custom hashing based search engine built in Java utilizing separate chaining and stacks. Allows user to enter input expressions in Postfix notation to search the document for a corresponding URL." link="https://github.com/jaredt17/CacheSimulator" imageLink={hashPicture} />
+                </Col>
+            </Row>
 
             </Container>
             
--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -8,13 +8,13 @@ import {
 class Preview extends Component { //needs title, subtitle, text, and a link to github if available
     render() {
         return (
-            <Card body inverse style={{backgroundColor: '#333', borderColor: '#333', margin: '5px'}}>
-                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+            <Card inverse style={{backgroundColor: '#333', borderColor: '#333', marginBottom: '10px'}}>
+                <CardImg top width="100%" src={this.props.imageLink} alt="No Image" />
                 <CardBody>
-                    <CardTitle>{this.props.title}</CardTitle>
+                    <CardTitle style={{fontWeight: "bold"}} className="text-center">{this.props.title}</CardTitle>
                     <CardSubtitle>{this.props.subtitle}</CardSubtitle>
                     <CardText>{this.props.text}</CardText>
-                    <CardLink href={this.props.link}>View on Github</CardLink>
+                    <Button href={this.props.link}>View on Github</Button>
                 </CardBody>
             </Card>
         );
