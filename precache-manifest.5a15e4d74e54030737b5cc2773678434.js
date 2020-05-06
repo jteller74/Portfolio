937640b53a240d088dfe4fad5538641df79748bb
Jared Teller <17jaredt17@gmail.com>
2020-05-05 21:22:50 -0400

    Updates

---
+++ precache-manifest.5a15e4d74e54030737b5cc2773678434.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70cc0644db03c5f9a98adbcb8e8601d4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "51af7133e2985d00723e",
+    "url": "/Portfolio/static/css/2.560c6c4d.chunk.css"
+  },
+  {
+    "revision": "0a870be55a7700270807",
+    "url": "/Portfolio/static/css/main.fc065aa3.chunk.css"
+  },
+  {
+    "revision": "51af7133e2985d00723e",
+    "url": "/Portfolio/static/js/2.74217bde.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.74217bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a870be55a7700270807",
+    "url": "/Portfolio/static/js/main.9b9f9efb.chunk.js"
+  },
+  {
+    "revision": "e6ca1522cb4810d23724",
+    "url": "/Portfolio/static/js/runtime-main.bd19731d.js"
+  },
+  {
+    "revision": "67be21f6505278ee8e26044d5c868311",
+    "url": "/Portfolio/static/media/hashmap.67be21f6.png"
+  },
+  {
+    "revision": "aa862a2864fef07c3b810a5062d42d19",
+    "url": "/Portfolio/static/media/hisserproj.aa862a28.png"
+  },
+  {
+    "revision": "a6872b463bd1253948ef6892b8768597",
+    "url": "/Portfolio/static/media/pic01.a6872b46.jpg"
+  },
+  {
+    "revision": "8887e231f6c1c8561a768e8e374a5a51",
+    "url": "/Portfolio/static/media/react.8887e231.jpg"
+  },
+  {
+    "revision": "9c830c2bf391368aa303212997d0d524",
+    "url": "/Portfolio/static/media/shellthumbnail.9c830c2b.jpg"
+  },
+  {
+    "revision": "cac1698f778abbb2497345a28b7e7acd",
+    "url": "/Portfolio/static/media/snackproj.cac1698f.png"
+  },
+  {
+    "revision": "e3150fb40f36d9bee933d67f71467380",
+    "url": "/Portfolio/static/media/wmulogo.e3150fb4.jpg"
+  },
+  {
+    "revision": "12bbc1cee70c10f72b71d1a101cc56c5",
+    "url": "/Portfolio/static/media/youtube.12bbc1ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2c74b69abb0640e37650d515cb5ce4f.js"
+  "/Portfolio/precache-manifest.5a15e4d74e54030737b5cc2773678434.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
